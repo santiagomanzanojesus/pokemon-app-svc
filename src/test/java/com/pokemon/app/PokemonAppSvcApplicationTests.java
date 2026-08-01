891e2856f8678bbaaefe6f6a8895cd14770eb73b
jesussmanzano <susej.sm@gmail.com>
2026-07-31 19:44:15 -0600

    initial commit

---
+++ src/test/java/com/pokemon/app/PokemonAppSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemon.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAppSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
